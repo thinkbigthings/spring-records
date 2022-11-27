dea48ff3126139ea1492721cc562c3809b012928
Jason Young <thinkbigthings@gmail.com>
2022-11-27 16:08:11 -0500

    added a little joke

--- src/main/java/com/thinkbigthings/springrecords/user/NetworkResult.java
+++ src/main/java/com/thinkbigthings/springrecords/user/NetworkResult.java
@@ -4,6 +4,7 @@ package com.thinkbigthings.springrecords.user;
 // if all subtypes are in this file then we do not need "permits"
 sealed interface NetworkResult<V> {
 
+    // Java Records! What do they do? Do they do things? Let's find out!
     record Success<V>(V result) implements NetworkResult<V> { }
     record Failure<V>(Throwable cause) implements NetworkResult<V> { }
     record Timeout<V>() implements NetworkResult<V> { }
