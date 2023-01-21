f453cba371f9c117688a45c0ace0d29d92983c1c
Jason Young <thinkbigthings@gmail.com>
2023-01-21 16:49:37 -0500

    added comments

--- src/main/java/com/thinkbigthings/springrecords/dto/UserAddress.java
+++ src/main/java/com/thinkbigthings/springrecords/dto/UserAddress.java
@@ -4,8 +4,8 @@ import static java.lang.String.join;
 
 public record UserAddress(String line1, String city, String state, String zip) {
 
-    // compact constructor is intended for validation
-    // called from constructor
+    // optional compact constructor is intended for validation
+    // always automatically called from the canonical constructor
     public UserAddress {
         if(line1.isBlank() || city.isBlank() || state.isBlank() || zip.isBlank() ) {
             throw new IllegalArgumentException("Data is missing, found: " + join(", ", line1, city, state, zip));
